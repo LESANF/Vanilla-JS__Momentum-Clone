ee6c1ecd2e415d2f4ff8e3b63217cc1f264dc07f
LESANF <nagong1000@naver.com>
2019-11-24 01:26:11 +0900

    background-Image__END

--- bg.js
+++ bg.js
@@ -1,10 +1,15 @@
 const body = document.querySelector("body");
 const IMG_NUMBER = 5;
 
-function paintImage(imgNumber) {}
+function paintImage(imgNumber) {
+  const backImage = new Image();
+  backImage.src = `images/${imgNumber}.jpg`;
+  backImage.classList.add("bgImage");
+  body.appendChild(backImage);
+}
 
 function genRandom() {
-  const number = Math.floor(Math.random() * IMG_NUMBER);
+  const number = Math.floor(Math.random() * IMG_NUMBER + 1);
   return number;
 }
 
